64d56212b3705d9c241454f9c7496ff5f106e2d7
James Murty <james@MisterM.org>
2010-07-15 20:25:10 -0700

    Clarified documentation for ServiceUtils#computeMD5Hash(InputStream) method

--- src/org/jets3t/service/utils/ServiceUtils.java
+++ src/org/jets3t/service/utils/ServiceUtils.java
@@ -509,6 +509,7 @@ public class ServiceUtils {
 
     /**
      * Computes the MD5 hash of the data in the given input stream and returns it as a hex string.
+     * The provided input stream is consumed and closed by this method.
      *
      * @param is
      * @return
