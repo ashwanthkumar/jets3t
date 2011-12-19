99ecb08fadefb43c117115a0984ef5efecaf0934
James Murty <james@MisterM.org>
2011-12-19 18:42:57 +1100

    Potential fix for issue #127 where NoHttpResponseException errors (caused by service-terminated requests) are not retried by JetS3t

--- src/org/jets3t/service/utils/RestUtils.java
+++ src/org/jets3t/service/utils/RestUtils.java
@@ -43,6 +43,7 @@ import org.apache.http.HttpRequest;
 import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
+import org.apache.http.NoHttpResponseException;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.AuthState;
 import org.apache.http.auth.Credentials;
@@ -654,7 +655,9 @@ public class RestUtils {
         public boolean retryRequest(IOException exception,
                 int executionCount,
                 HttpContext context) {
-            if (super.retryRequest(exception, executionCount, context)){
+            if (super.retryRequest(exception, executionCount, context)
+                || (exception instanceof NoHttpResponseException))
+            {
 
                 if (exception instanceof UnrecoverableIOException) {
                     if (log.isDebugEnabled()) {
