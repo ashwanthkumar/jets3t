8218d3534821b9e3fb81bc171915338ccf3625e9
James Murty <james@MisterM.org>
2011-12-20 20:56:20 +1100

    Another potential fix for issue #127: convert RequestWrapper object into underlying HttpRequestBase (hopefully)

--- src/org/jets3t/service/utils/RestUtils.java
+++ src/org/jets3t/service/utils/RestUtils.java
@@ -65,6 +65,7 @@ import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.AbstractHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.RequestWrapper;
 import org.apache.http.impl.conn.tsccm.AbstractConnPool;
 import org.apache.http.impl.conn.tsccm.ConnPoolByRoute;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
@@ -664,6 +665,11 @@ public class RestUtils {
                 }
                 HttpRequest request = (HttpRequest) context.getAttribute(
                         ExecutionContext.HTTP_REQUEST);
+
+                // Convert RequestWrapper to original HttpBaseRequest (issue #127)
+                if (request instanceof RequestWrapper) {
+                    request = ((RequestWrapper)request).getOriginal();
+                }
 
                 if (!(request instanceof HttpRequestBase)) {
                     return false;
