8c4597842e58fb52859f0bdd150b7f5f04d2c521
David Kocher <dkocher@sudo.ch>
2012-10-01 13:58:05 +0200

    Use java.util.UUID.

--- src/org/jets3t/servlets/gatekeeper/impl/ExternalUuidProvider.java
+++ src/org/jets3t/servlets/gatekeeper/impl/ExternalUuidProvider.java
@@ -18,14 +18,13 @@
  */
 package org.jets3t.servlets.gatekeeper.impl;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-
 import org.jets3t.service.utils.gatekeeper.GatekeeperMessage;
 import org.jets3t.servlets.gatekeeper.ClientInformation;
 import org.jets3t.servlets.gatekeeper.TransactionIdProvider;
-import org.safehaus.uuid.UUID;
-import org.safehaus.uuid.UUIDGenerator;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import java.util.UUID;
 
 /**
  * Uses an externally provided UUID obtained from an application property <tt>externalUUID</tt>
@@ -44,16 +43,14 @@ public class ExternalUuidProvider extends TransactionIdProvider {
      * is returned as the transaction ID. If the property is not availble a new random-based
      * UUID is generated using the JUG library.
      */
-    public String getTransactionId(GatekeeperMessage requestMessage, ClientInformation clientInformation)
-    {
+    public String getTransactionId(GatekeeperMessage requestMessage, ClientInformation clientInformation) {
         // Use the external UUID as a transaction ID, if it's available.
         String externalUuid = requestMessage.getApplicationProperties().getProperty("externalUUID");
-        if (externalUuid != null && externalUuid.length() > 0) {
+        if(externalUuid != null && externalUuid.length() > 0) {
             return externalUuid;
         }
         // Use a generated UUID based on a random generation as a fallback if the external UUID isn't available.
-        UUID uuid = UUIDGenerator.getInstance().generateRandomBasedUUID();
-        return uuid.toString();
+        return UUID.randomUUID().toString();
     }
 
 }
