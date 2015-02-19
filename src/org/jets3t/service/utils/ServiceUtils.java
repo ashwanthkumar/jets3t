35009de6e8b738e67173d4c8a3a79f665289f78e
David Kocher <dkocher@sudo.ch>
2015-02-19 11:58:46 +0100

    Revert commit 9610149.

--- src/org/jets3t/service/utils/ServiceUtils.java
+++ src/org/jets3t/service/utils/ServiceUtils.java
@@ -836,11 +836,6 @@ public class ServiceUtils {
         String path = uri.getPath();
         String[] pathSplit = ServiceUtils.splitIgnoreEmpty(path, "/");
 
-        // Guard against null host value, per #205
-        if (host == null) {
-            host = "";
-        }
-
         // Handle case where Host exactly matches endpoint, so no chance of it
         // being a virtual host or alternate host name
         if (host.equalsIgnoreCase(s3Endpoint)) {
